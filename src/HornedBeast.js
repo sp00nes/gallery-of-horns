8a969746bf3619564a51698ad4e8e029d58b8edd
MARTIN HANSEN <marrtt1234@gmail.com>
2022-11-08 23:52:42 +0000

    imgs populate

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,15 +4,11 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <article>
-        <h3>{this.props.name}</h3>
-        <p>👋 {this.state.greetings} greetings</p>
-        <p onClick={this.handleGreeting}>Say Hello!</p>
+        <h2>{this.props.name}</h2>
         <img
           src={this.props.imageURL}
           alt={this.props.name}
         />
-        {/* WTF */}
-        <div>{this.state.helpMe ? 'I need help' : ''}</div>
       </article>
     )
   }
