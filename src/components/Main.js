227e60a442cee163f8f06b8a23fe4aac3eddff89
MARTIN HANSEN <marrtt1234@gmail.com>
2022-11-09 20:28:20 +0000

    huh?

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,20 +6,40 @@ import './Main.css';
 class Main extends React.Component {
   render() {
     //makes an array of Horned Beasts that have data from the json file.
-    let beastArray = [];
-    data.forEach((beastData, idx) => {
-      beastArray.push(
-        <HornedBeast
-          name={beastData.title}
-          imageURL={beastData.image_url}
-          info={beastData.description}
-          key={idx}
-        />
-      )
-    });
+    let renderAll = () => {
+      let beastArray = [];
+      data.forEach((beastData, idx) => {
+        beastArray.push(
+          <HornedBeast
+            name={beastData.title}
+            imageURL={beastData.image_url}
+            info={beastData.description}
+            key={idx}
+          />
+        )
+      });
+      return beastArray;
+    }
+
+    let renderFav = () => {
+      let beastArray = [];
+      data.forEach((beastData, idx) => {
+        if (beastData.fav) {
+          beastArray.push(
+            <HornedBeast
+              name={beastData.title}
+              imageURL={beastData.image_url}
+              info={beastData.description}
+              key={idx}
+            />
+          )
+        }
+      });
+      return beastArray;
+    }
     return (
       <main>
-       {beastArray}
+        {renderAll()}
       </main>
     )
   }
